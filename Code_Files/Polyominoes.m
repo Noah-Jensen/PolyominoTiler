6d3eedcb309c06361b2533e385cb889233b9d00b
Noah-Jensen <123452253+Noah-Jensen@users.noreply.github.com>
2024-08-25 23:07:34 -0700

    Update Polyominoes.m

--- Code_Files/Polyominoes.m
+++ Code_Files/Polyominoes.m
@@ -2,10 +2,10 @@ function PolyominoMatrix = Polyominoes
 %PolyominoMatrix = Polyominoes
 %
 %   A file used to define the polyominoes that will be used to tile the
-%   defined board. This is a m x n x r tensor where r is the number of
+%   defined board. This is a list of r m x n matrices where r is the number of
 %   polyominoes that you have. Each polyomino is stored in an m x n matrix
 %   and need not fill the entire matrix.
 
-PolyominoMatrix = [ 1 1 ; 1 0 ];
+PolyominoMatrix = zeroes(m,n,r);
 
 end
